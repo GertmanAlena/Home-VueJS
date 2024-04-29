7f5b40760addf8bfd765a87a805bb469f055488c
Alena Hertman <gertmanea@mail.ru>
2024-04-29 20:45:51 +0300

    "страницу Blog сделала и дз VUe"

---
+++ scriptBlog.js
@@ -0,0 +1,60 @@
+
+new Vue({
+    el: '#app',
+    data: {
+
+        title: 'Articles & News',
+        articles: [
+            {
+                id: 1,
+                stick: 'Kitchan Design',
+                image: './images/1.1.png',
+                title: 'Let’s Get Solution For Building Construction Work',
+                data: '26 December,2022',
+            },
+            {
+                id: 2,
+                stick: 'Living Design',
+                image: './images/2.2.png',
+                title: 'Let’s Get Solution For Building Construction Work',
+                data: '26 December,2022',
+            },
+            {
+                id: 3,
+                stick: 'Interior Design',
+                image: './images/3.3.png',
+                title: 'Best For Any Office & Business Interior Solution',
+                data: '26 December,2022',
+            },
+            {
+                id: 4,
+                stick: 'Kitchan Design',
+                image: './images/4.4.png',
+                title: 'Let’s Get Solution For Building Construction Work',
+                data: '26 December,2022',
+            },
+            {
+                id: 5,
+                stick: 'Living Design',
+                image: './images/5.5.png',
+                title: 'Let’s Get Solution For Building Construction Work',
+                data: '26 December,2022',
+            },
+            {
+                id: 6,
+                stick: 'Interior Design',
+                image: './images/6.6.png',
+                title: 'Best For Any Office & Business Interior Solution',
+                data: '26 December,2022',
+            }
+        ],
+    className: [
+        {
+            articles__news: 'articles__news',
+            heading: 'heading'
+        }
+
+    ]
+}
+});
+
